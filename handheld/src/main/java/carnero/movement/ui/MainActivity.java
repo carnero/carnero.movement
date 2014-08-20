c45be2511dc501c9dd56701736a269843ce1794b
carnero <carnero@inmite.eu>
2014-08-20 23:53:53 +0200

    ActionBar graph

--- handheld/src/main/java/carnero/movement/ui/MainActivity.java
+++ handheld/src/main/java/carnero/movement/ui/MainActivity.java
@@ -31,6 +31,9 @@ public class MainActivity extends AbstractBaseActivity {
     private Helper mHelper;
     private Line mLineDistance;
     //
+    private static final int HISTORY_PAGES = 7;
+    private static final int GRAPH_DAYS = 21;
+    //
     @InjectView(R.id.pager)
     ViewPager vPager;
     SmoothLineGraph vGraph;
@@ -70,6 +73,7 @@ public class MainActivity extends AbstractBaseActivity {
 
         mPagerAdapter = new PagesAdapter();
         vPager.setAdapter(mPagerAdapter);
+        vPager.setOnPageChangeListener(new PageChangeListener());
         vPager.setCurrentItem(mPagerAdapter.getCount() - 1);
     }
 
@@ -93,27 +97,43 @@ public class MainActivity extends AbstractBaseActivity {
 
     private class PagesAdapter extends FragmentStatePagerAdapter {
 
-        private final int mPages = 7;
-
         public PagesAdapter() {
             super(getSupportFragmentManager());
         }
 
         @Override
         public Fragment getItem(int position) {
-            return GraphFragment.newInstance(position - mPages + 1);
+            return GraphFragment.newInstance(position - HISTORY_PAGES + 1);
         }
 
         @Override
         public int getCount() {
-            return mPages;
+            return HISTORY_PAGES;
+        }
+    }
+
+    private class PageChangeListener implements ViewPager.OnPageChangeListener {
+
+        @Override
+        public void onPageSelected(int i) {
+            if (vGraph != null) {
+                vGraph.setHighlightSegment(GRAPH_DAYS - (HISTORY_PAGES - i - 1));
+            }
+        }
+
+        @Override
+        public void onPageScrolled(int i, float v, int i2) {
+            // empty
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int i) {
+            // empty
         }
     }
 
     private class GraphDataTask extends BaseAsyncTask {
 
-        private final int mDays = 21;
-        //
         private float mMinDst = Float.MAX_VALUE;
         private float mMaxDst = Float.MIN_VALUE;
 
@@ -121,18 +141,18 @@ public class MainActivity extends AbstractBaseActivity {
         public void inBackground() {
             mLineDistance.getPoints().clear();
 
-            for (int i = -mDays; i <= 0; i ++) {
+            for (int i = -GRAPH_DAYS; i <= 0; i ++) {
                 ModelData summary = mHelper.getSummaryForDay(i);
                 LinePoint pointDistance = new LinePoint();
 
                 if (summary == null) {
-                    pointDistance.setX(mDays + i);
+                    pointDistance.setX(GRAPH_DAYS + i);
                     pointDistance.setY(0);
 
                     mMinDst = Math.min(mMinDst, 0f);
                     mMaxDst = Math.max(mMaxDst, 0f);
                 } else {
-                    pointDistance.setX(mDays + i);
+                    pointDistance.setX(GRAPH_DAYS + i);
                     pointDistance.setY(summary.distance);
 
                     mMinDst = Math.min(mMinDst, summary.distance);
